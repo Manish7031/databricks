ff9d4659f0d044a6837ee75507821fa994cdbc83
Manish7031 <singh.7031@gmail.com>
2026-01-17 04:23:21 +0000

    spark commit 3.6

---
+++ databricks-notebook/Autoloaders_delta_live_tables_notebook_03.py
@@ -0,0 +1,132 @@
+# Databricks notebook source
+import dlt
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### create streaming table
+
+# COMMAND ----------
+
+@dlt.table(
+    table_properties = {"quality": "bronze"},
+    comment = "bronze table"
+)
+def order_bronze():
+    df = spark.readStream.table("devenv.bronze.orders_raw")
+    return df
+
+# COMMAND ----------
+
+## create stream table for order -autoloaders
+@dlt.table(
+    table_properties = {"quality": "bronze"},
+    comment = "order autoloader table",
+    name = "orders_autoloader_bronze"
+)
+def func():
+    df = (
+        spark.readStream
+             .format("cloudFiles")
+             .option("cloudFiles.schemaHints", o_orderkey long, o_custkey long, o_orderstatus string, o_totalprice double, o_orderdate date, o_orderpriority string, o_clerk string, o_shippriority integer, o _comment string")
+             .option("cloudFiles.format", "csv")
+             .option("pathGlobfilter", "*.csv"),
+             .option("cloudFiles.schemaLocation", "/Volumes/devenv/etl/landing/autoloader/schema/1/"),
+             .option("schemaEvolutionMode", "none"),
+             .load("/Volumes/devenv/etl/landing/files/")
+    
+    
+    
+    )
+return df
+
+# COMMAND ----------
+
+@dlt.create_streaming_table("orders_union_bronze")
+
+@dlt.append_flow(
+    target = "orders_union_bronze"
+)
+def order_delta_append():
+    df = spark.readStream.table("LIVE.order_bronze")
+    return df
+
+@dlt.append_flow(
+    target = "orders_union_bronze"
+)
+def order_autoloader_append():
+    df = spark.readStream.table("LIVE.orders_autoloader_bronze")
+    return df
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### create materialized view
+
+# COMMAND ----------
+
+@dlt.table(
+    table_properties = {"quality": "bronze"},
+    comment = "customer bronze table",
+    name = "customer_bronze"
+)
+def cust_bronze():
+    df = spark.read.table("devenv.bronze.customer_raw")
+    return df
+
+# COMMAND ----------
+
+### create DLT views
+
+@dlt.view(
+    comment = "Joined view"
+)
+def joined_vw():
+    df_c = spark.read.table("LIVE.customer_bronze")
+    df_o = spark.read.table("LIVE.orders_union_bronze")
+    df_join = df_o.join(
+        df_c,
+        how ="left_outer",
+        on = df_c.c_custkey == df_o.o_custkey
+    )
+    
+    return df_join
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### create silver layer table
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, count
+@dlt.table(
+    table_properties = {"quality": "silver"},
+    comment = "silver table",
+    name = "joined_silver"
+)
+def joined_silver():
+    df = spark.readStream.table("LIVE.joined_vw").withcolumn("ingesttime", current_timestamp())
+    return df
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### gold table
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, count
+@dlt.table(
+    table_properties = {"quality": "gold"},
+    comment = "orders aggregated table"
+)
+def orders_agg_gold():
+    df = spark.readStream.table("LIVE.joined_silver")
+    
+    final_df = df.groupBy("c_mktsegment")
+                 .agg(count(("o_orderkey")
+                            .alias("sum_orders")))
+                      .withcolumn("ingesttime", current_timestamp())
+    return final_df
